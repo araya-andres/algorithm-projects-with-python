156c98ad125a6fcd1e96653dc1d3bfef9bf31b01
Andres Araya <araya.andres@gmail.com>
2022-04-12 19:34:29 -0600

    add annotations and apply formatter

--- BinaryNode.py
+++ BinaryNode.py
@@ -1,41 +1,47 @@
-def binary_tree_as_str(node, level):
-    indent = '  ' * level
+from __future__ import annotations
+
+
+def binary_tree_as_str(node: BinaryNode, level: int) -> str:
+    indent = "  " * level
     if node is None:
-        return f'{indent}{node}'
+        return f"{indent}{node}"
     elif node.has_children():
         left_child = binary_tree_as_str(node.left_child, level + 1)
-        right_child = binary_tree_as_str(node.right_child, level + 1) 
-        return f'{indent}{node.value}:\n{left_child}\n{right_child}'
+        right_child = binary_tree_as_str(node.right_child, level + 1)
+        return f"{indent}{node.value}:\n{left_child}\n{right_child}"
     else:
-        return f'{indent}{node.value}:'
+        return f"{indent}{node.value}:"
 
 
 class BinaryNode:
-    def __init__(self, value, left_child = None, right_child = None):
+    def __init__(
+        self, value, left_child: BinaryNode = None, right_child: BinaryNode = None
+    ):
         self.value = value
         self.left_child = left_child
         self.right_child = right_child
 
-    def add_left(self, left):
+    def add_left(self, left: BinaryNode):
         self.left_child = left
 
-    def add_right(self, right):
+    def add_right(self, right: BinaryNode):
         self.right_child = right
 
-    def has_children(self):
+    def has_children(self) -> bool:
         return self.left_child or self.right_child
 
-    def __str__(self):
+    def __str__(self) -> str:
         return binary_tree_as_str(self, 0)
 
 
 def main():
-    root = BinaryNode('Root',
-        BinaryNode('A', BinaryNode('C'), BinaryNode('D')),
-        BinaryNode('B', None, BinaryNode('E', BinaryNode('F')))
+    root = BinaryNode(
+        "Root",
+        BinaryNode("A", BinaryNode("C"), BinaryNode("D")),
+        BinaryNode("B", None, BinaryNode("E", BinaryNode("F"))),
     )
     print(root)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- NaryNode.py
+++ NaryNode.py
@@ -1,42 +1,60 @@
-def nary_tree_as_str(node, level):
-    indent = '  ' * level
+from __future__ import annotations
+from typing import List
+
+
+def nary_tree_as_str(node: NaryNode, level: int) -> str:
+    indent = "  " * level
     if node is None:
-        return f'{indent}{node}'
+        return f"{indent}{node}"
     elif len(node.children) > 0:
-        children = '\n'.join(nary_tree_as_str(child, level + 1) for child in node.children)
-        return f'{indent}{node.value}:\n{children}'
+        children = "\n".join(
+            nary_tree_as_str(child, level + 1) for child in node.children
+        )
+        return f"{indent}{node.value}:\n{children}"
     else:
-        return f'{indent}{node.value}:'
+        return f"{indent}{node.value}:"
 
 
 class NaryNode:
-    def __init__(self, value, children=[]):
+    def __init__(self, value, children: List[NaryNode] = []):
         self.value = value
         self.children = children
 
-    def add_child(self, child_node):
+    def add_child(self, child_node: NaryNode):
         self.children.append(child_node)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return nary_tree_as_str(self, 0)
 
 
 def main():
-    root = NaryNode('Root', [
-        NaryNode('A', [
-            NaryNode('D'),
-            NaryNode('E'),
-        ]),
-        NaryNode('B'),
-        NaryNode('C', [
-            NaryNode('F', [
-                NaryNode('H'),
-                NaryNode('I'),
-            ])
-        ]),
-    ])
+    root = NaryNode(
+        "Root",
+        [
+            NaryNode(
+                "A",
+                [
+                    NaryNode("D"),
+                    NaryNode("E"),
+                ],
+            ),
+            NaryNode("B"),
+            NaryNode(
+                "C",
+                [
+                    NaryNode(
+                        "F",
+                        [
+                            NaryNode("H"),
+                            NaryNode("I"),
+                        ],
+                    )
+                ],
+            ),
+        ],
+    )
     print(root)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
