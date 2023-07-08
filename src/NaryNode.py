b870f0d05401367ec42005618f7a05d6a97d5bff
Andres Araya <araya.andres@gmail.com>
2023-07-08 14:24:25 -0600

    basic tk framework for nary node

--- src/NaryNode.py
+++ src/NaryNode.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import List
+import tkinter as tk
 
 
 def nary_tree_as_str(node: NaryNode, level: int) -> str:
@@ -16,6 +17,8 @@ def nary_tree_as_str(node: NaryNode, level: int) -> str:
 
 
 class NaryNode:
+    node_radius = 8  # FIXME
+
     def __init__(self, value, children: List[NaryNode] = []):
         self.value = value
         self.children = children
@@ -51,6 +54,84 @@ class NaryNode:
     def __str__(self) -> str:
         return nary_tree_as_str(self, 0)
 
+    def arrange_subtree(self, xmin, ymin):
+        """Position the node's subtree."""
+        # Calculate cy, the Y coordinate for this node.
+        # This doesn't depend on the children.
+        # ...
+
+        # If the node has no children, just place it here and return.
+        if len(self.children) == 0:
+            # ...
+            return
+
+        # Set child_xmin and child_ymin to the
+        # start position for child subtrees.
+        # ...
+
+        # Set ymax equal to the largest Y position used.
+        ymax = ymin + 2 * NaryNode.node_radius
+
+        # Position the child subtrees.
+        for child in self.children:
+            # ...
+            # Position this child subtree.
+            ...
+
+            # Update child_xmin to allow room for the subtree
+            # and space between the subtrees.
+            ...
+
+            # Update the subtree bottom ymax.
+            # ...
+
+        # Set xmax equal to child_xmin minus the horizontal
+        # spacing we added after the last subtree.
+        # ...
+
+        # Use xmin, ymin, xmax, and ymax to set our subtree bounds.
+        # ...
+
+        # Center this node over the subtree bounds.
+        # ...
+
+    def draw_subtree_links(self, canvas):
+        """Draw the subtree's links."""
+        # If we have exactly one child, just draw to it.
+        if len(self.children) == 1:
+            # ...
+            pass
+
+        # Else if we have more than one child,
+        # draw vertical and horizontal branches.
+        elif len(self.children) > 0:
+            # ...
+            pass
+
+        # Recursively draw child subtree links.
+        # ...
+
+        # Outline the subtree for debugging.
+        # canvas.create_rectangle(self.subtree_bounds, fill='', outline='red')
+
+    def draw_subtree_nodes(self, canvas):
+        """Draw the subtree's nodes."""
+        # Draw the node.
+        # ...
+
+        # Draw the descendants' nodes.
+        # ...
+
+    def arrange_and_draw_subtree(self, canvas, xmin, ymin):
+        # Position the tree.
+        self.arrange_subtree(xmin, ymin)
+
+        # Draw the links.
+        self.draw_subtree_links(canvas)
+
+        # Draw the nodes.
+        self.draw_subtree_nodes(canvas)
+
 
 def find_value(root: NaryNode, value) -> None:
     if root.find_node(value):
@@ -59,61 +140,46 @@ def find_value(root: NaryNode, value) -> None:
         print(f"Value {value} not found")
 
 
-def main():
-    root = NaryNode(
-        "Root",
-        [
-            NaryNode(
-                "A",
-                [
-                    NaryNode(
-                        "D",
-                        [
-                            NaryNode("G"),
-                        ],
-                    ),
-                    NaryNode("E"),
-                ],
-            ),
-            NaryNode("B"),
-            NaryNode(
-                "C",
-                [
-                    NaryNode(
-                        "F",
-                        [
-                            NaryNode("H"),
-                            NaryNode("I"),
-                        ],
-                    )
-                ],
-            ),
-        ],
-    )
-    print(root)
-    print(root.children[0])
-    print()
-    # Find some values.
-    find_value(root, "Root")
-    find_value(root, "E")
-    find_value(root, "F")
-    find_value(root, "Q")
+a = NaryNode("A")
+b = NaryNode("B")
+c = NaryNode("C")
+d = NaryNode("D")
+e = NaryNode("E")
+f = NaryNode("F")
+g = NaryNode("G")
+h = NaryNode("H")
+i = NaryNode("I")
+j = NaryNode("J")
+k = NaryNode("K")
 
-    print("Preorder:  ", end="")
-    for node in root.traverse_preorder():
-        print(f"{node.value} ", end="")
-    print()
-
-    print("Postorder:  ", end="")
-    for node in root.traverse_postorder():
-        print(f"{node.value} ", end="")
-    print()
-
-    print("Breadth-First:  ", end="")
-    for node in root.traverse_breadth_first():
-        print(f"{node.value} ", end="")
-    print()
+a.add_child(b)
+a.add_child(c)
+a.add_child(d)
+b.add_child(e)
+b.add_child(f)
+d.add_child(g)
+e.add_child(h)
+g.add_child(i)
+g.add_child(j)
+g.add_child(k)
 
 
-if __name__ == "__main__":
-    main()
+def kill_callback():
+    """A callback to destroy the tkinter window."""
+    window.destroy()
+
+
+# Make the tkinter window.
+window = tk.Tk()
+window.title("nary_node5")
+window.protocol("WM_DELETE_WINDOW", kill_callback)
+window.geometry("260x180")
+
+canvas = tk.Canvas(window, bg="white", borderwidth=2, relief=tk.SUNKEN)
+canvas.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+# Draw the tree.
+a.arrange_and_draw_subtree(canvas, 10, 10)
+
+window.focus_force()
+window.mainloop()
