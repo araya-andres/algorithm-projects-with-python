881b08e018e29bdec1705384585c2953826e4d71
Andres Araya <araya.andres@gmail.com>
2023-07-22 12:40:20 +0000

    make load from file more robust

--- 2-shortest-paths/serializer.py
+++ 2-shortest-paths/serializer.py
@@ -3,6 +3,10 @@ from network import Link, Network, Node
 COMMENT = "#"
 
 
+class DeserializationException(Exception):
+    pass
+
+
 def _node_to_string(node: Node) -> str:
     return f"{node.pos_x},{node.pos_y},{node.text}"
 
@@ -30,13 +34,26 @@ def save_into_file(network, filename: str):
 
 
 def _add_node(network: Network, node_str: str) -> Node:
-    pos_x, pos_y, text = node_str.split(",")
-    return network.add_node(int(pos_x), int(pos_y), text)
+    try:
+        pos_x, pos_y, text = node_str.split(",")
+        return network.add_node(int(pos_x), int(pos_y), text)
+    except ValueError:
+        raise DeserializationException(f"Invalid node string: '{node_str}'")
 
 
 def _add_link(network: Network, link_str: str) -> Link:
-    from_index, to_index, cost = (int(value) for value in link_str.split(","))
-    return network.add_link(network.nodes[from_index], network.nodes[to_index], cost)
+    if len(network.nodes) == 0:
+        raise DeserializationException("Can not have a link in an empty network")
+    try:
+        from_index, to_index, cost = (int(value) for value in link_str.split(","))
+        n_nodes = len(network.nodes)
+        if not (0 <= from_index < n_nodes and 0 <= to_index < n_nodes):
+            raise DeserializationException(f"Node index out of bounds: '{link_str}'")
+        from_node = network.nodes[from_index]
+        to_node = network.nodes[to_index]
+        return network.add_link(from_node, to_node, cost)
+    except ValueError:
+        raise DeserializationException(f"Invalid link string: '{link_str}'")
 
 
 def _parse(line: str) -> str:
@@ -49,16 +66,42 @@ def _parse(line: str) -> str:
 def load_from_file(filename: str) -> Network:
     network = Network()
     with open(filename, "r", encoding="utf-8") as reader:
-        num_nodes = int(_parse(reader.readline()))
-        num_links = int(_parse(reader.readline()))
+        num_nodes = 0
+        while True:
+            if line := reader.readline():
+                if parsed_line := _parse(line):
+                    num_nodes = int(parsed_line)
+                    break
+            else:
+                raise DeserializationException("Could not find the number of nodes")
+        num_links = 0
+        while True:
+            if line := reader.readline():
+                if parsed_line := _parse(line):
+                    num_links = int(parsed_line)
+                break
+            else:
+                raise DeserializationException("Could not find the number of links")
         i = 0
-        while i < num_nodes:
-            if line := _parse(reader.readline()):
-                _add_node(network, line)
-                i += 1
+        if num_nodes == 0 and num_links > 0:
+            raise DeserializationException("Can not have a link in an empty network")
+        while True:
+            if line := reader.readline():
+                if node_str := _parse(line):
+                    _add_node(network, node_str)
+                    i += 1
+                    if i >= num_nodes:
+                        break
+            else:
+                raise DeserializationException()
         i = 0
-        while i < num_links:
-            if line := _parse(reader.readline()):
-                _add_link(network, line)
-                i += 1
+        while True:
+            if line := reader.readline():
+                if link_str := _parse(line):
+                    _add_link(network, link_str)
+                    i += 1
+                    if i >= num_links:
+                        break
+            else:
+                raise DeserializationException()
     return network
--- 2-shortest-paths/serializer_tests.py
+++ 2-shortest-paths/serializer_tests.py
@@ -1,25 +1,27 @@
+import pytest
 import serializer
 from network import Network
+from serializer import DeserializationException
 
 
-def test_node_to_string():
-    node = Network().add_node(0, 1, "A")
-    assert serializer._node_to_string(node) == "0,1,A"
-
-
-def test_link_to_string():
+@pytest.fixture
+def network() -> Network:
     network = Network()
     node_a = network.add_node(0, 0, "A")
     node_b = network.add_node(1, 1, "B")
-    link_a_b = network.add_link(node_a, node_b, 10)
-    assert serializer._link_to_string(link_a_b) == "0,1,10"
+    network.add_link(node_a, node_b, 1)
+    return network
 
 
-def test_network_to_string():
-    network = Network()
-    node_a = network.add_node(0, 0, "A")
-    node_b = network.add_node(1, 1, "B")
-    network.add_link(node_a, node_b, 10)
+def test_node_to_string(network):
+    assert serializer._node_to_string(network.nodes[0]) == "0,0,A"
+
+
+def test_link_to_string(network):
+    assert serializer._link_to_string(network.links[0]) == "0,1,1"
+
+
+def test_network_to_string(network):
     assert serializer._network_to_string(network) == "\n".join(
         [
             "2 # Num nodes.",
@@ -28,37 +30,87 @@ def test_network_to_string():
             "0,0,A",
             "1,1,B",
             "# Links.",
-            "0,1,10",
+            "0,1,1",
         ]
     )
 
 
-def test_node_from_string():
-    network = Network()
-    node = serializer._add_node(network, "1,0,B")
-    assert node.index == 0
-    assert node.pos_x == 1
-    assert node.pos_y == 0
-    assert node.text == "B"
+def test_node_from_string(network):
+    node = serializer._add_node(network, "0,1,C")
+    assert node.index == 2
+    assert node.pos_x == 0
+    assert node.pos_y == 1
+    assert node.text == "C"
 
 
-def test_link_from_string():
-    network = Network()
-    network.add_node(0, 0, "A")
-    network.add_node(1, 1, "B")
-    link_str = "0,1,10"
-    link_a_b = serializer._add_link(network, link_str)
-    assert str(link_a_b) == "[A] --> [B] (10)"
+def test_parse_a_node_string_with_no_text_raises_an_exception(network):
+    with pytest.raises(DeserializationException) as ex:
+        serializer._add_node(network, "1,0")
+    assert str(ex.value) == "Invalid node string: '1,0'"
 
 
-def test_save_and_load_from_file():
-    network = Network()
-    node_a = network.add_node(0, 0, "A")
-    node_b = network.add_node(1, 1, "B")
-    network.add_link(node_a, node_b, 10)
+def test_parse_a_node_string_with_a_non_numerical_value_for_x_raises_an_exception(
+    network,
+):
+    with pytest.raises(DeserializationException) as ex:
+        serializer._add_node(network, "foo,0,bar")
+    assert str(ex.value) == "Invalid node string: 'foo,0,bar'"
+
+
+def test_parse_a_node_string_with_a_non_numerical_value_for_y_raises_an_exception(
+    network,
+):
+    with pytest.raises(DeserializationException) as ex:
+        serializer._add_node(network, "0,foo,bar")
+    assert str(ex.value) == "Invalid node string: '0,foo,bar'"
+
+
+def test_get_a_link_from_string(network):
+    link_str = "1,0,1"
+    link_b_a = serializer._add_link(network, link_str)
+    assert str(link_b_a) == "[B] --> [A] (1)"
+
+
+def test_parse_a_link_string_with_no_cost_raises_an_exception(network):
+    with pytest.raises(DeserializationException) as ex:
+        serializer._add_link(network, "0,1")
+    assert str(ex.value) == "Invalid link string: '0,1'"
+
+
+def test_parse_a_link_string_with_a_non_numeric_value_raises_an_exception(network):
+    with pytest.raises(DeserializationException) as ex:
+        serializer._add_link(network, "1,foo,1")
+    assert str(ex.value) == "Invalid link string: '1,foo,1'"
+
+
+def test_parse_a_link_string_with_index_out_of_bounds_raises_an_exception(network):
+    with pytest.raises(DeserializationException) as ex:
+        serializer._add_link(network, "1,2,3")
+    assert str(ex.value) == "Node index out of bounds: '1,2,3'"
+
+
+def test_save_and_load_from_file(network):
     filename = "test.net"
     serializer.save_into_file(network, filename)
     new_network = serializer.load_from_file(filename)
-    assert serializer._network_to_string(network) == serializer._network_to_string(
-        new_network
-    )
+    assert len(new_network.nodes) == 2
+    assert len(new_network.links) == 1
+    assert str(new_network.links[0]) == "[A] --> [B] (1)"
+
+
+def test_load_an_empty_file_raises_an_exception():
+    with pytest.raises(DeserializationException) as ex:
+        serializer.load_from_file("test-files/empty.txt")
+    assert str(ex.value) == "Could not find the number of nodes"
+
+
+def test_load_a_file_with_only_commented_lines_raises_an_exception():
+    with pytest.raises(DeserializationException) as ex:
+        serializer.load_from_file("test-files/only-commented-lines.txt")
+    assert str(ex.value) == "Could not find the number of nodes"
+
+
+def test_load_a_file_with_only_the_number_of_nodes_raises_an_exception():
+    with pytest.raises(DeserializationException) as ex:
+        serializer.load_from_file("test-files/only-node-num.txt")
+    assert str(ex.value) == "Could not find the number of links"
