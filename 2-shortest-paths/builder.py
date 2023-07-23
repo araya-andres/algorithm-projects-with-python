108a6053b24c7a95f00c5a570217ad5f2a915ef4
Andres Araya <araya.andres@gmail.com>
2023-07-23 21:44:35 +0000

    parse command line arguments

--- 2-shortest-paths/builder.py
+++ 2-shortest-paths/builder.py
@@ -1,9 +1,11 @@
 """
 Generate grid-shaped networks.
 """
+import argparse
 import random
 
 from network import Network, Node
+from serializer import save_into_file
 
 
 def _cost(lenght: int) -> int:
@@ -19,7 +21,8 @@ def build_grid_network(width: int, height: int, num_rows: int, num_cols) -> Netw
     network = Network()
 
     # Add nodes
-    _r = Node.LARGE_RADIUS if num_rows * num_cols < Network.BIG else Node.SMALL_RADIUS
+    num_nodes = num_rows * num_cols
+    _r = Node.LARGE_RADIUS if num_nodes < Network.BIG else Node.SMALL_RADIUS
     _dx = (width - 4 * _r) / (num_cols - 1) if num_cols > 1 else 0
     _dy = (height - 4 * _r) / (num_rows - 1) if num_rows > 1 else 0
     pos_x = 2 * _r
@@ -37,7 +40,7 @@ def build_grid_network(width: int, height: int, num_rows: int, num_cols) -> Netw
     col = 0
     row = 0
     nodes = network.nodes
-    for i in range(len(nodes)):
+    for i in range(num_nodes):
         if col > 0:
             network.add_link(nodes[i], nodes[i - 1], _cost(_dx))
         if col < num_cols - 1:
@@ -52,3 +55,19 @@ def build_grid_network(width: int, height: int, num_rows: int, num_cols) -> Netw
             row += 1
 
     return network
+
+
+def _main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    parser.add_argument("width", type=int)
+    parser.add_argument("height", type=int)
+    parser.add_argument("rows", type=int)
+    parser.add_argument("cols", type=int)
+    args = parser.parse_args()
+    network = build_grid_network(args.width, args.height, args.rows, args.cols)
+    save_into_file(network, args.filename)
+
+
+if __name__ == "__main__":
+    _main()
