511257607cc581c555ed6b84d336440ebd1c0fad
Andres Araya <araya.andres@gmail.com>
2023-08-06 00:47:00 +0000

    refactor turtle

--- 6-fractals/milestone1/hilbert_curve.py
+++ 6-fractals/milestone1/hilbert_curve.py
@@ -1,7 +1,7 @@
-import math
 import tkinter as tk
 
 from common.point import Point
+from common.turtle import Turtle
 
 
 def get_string(n: int = 1) -> str:
@@ -13,26 +13,6 @@ def get_string(n: int = 1) -> str:
     s = s.replace("a", "")
     s = s.replace("b", "")
     return s
-
-
-class Turtle:
-    def __init__(self):
-        self.delta = [
-            Point(0, 1),  # North
-            Point(-1, 0),  # West
-            Point(0, -1),  # South
-            Point(1, 0),  # East
-        ]
-        self.idx = 0
-
-    def turn(self, direction: str = "") -> None:
-        if direction == "+":
-            self.idx = (self.idx + 1) % len(self.delta)
-        elif direction == "-":
-            self.idx = (len(self.delta) + self.idx - 1) % len(self.delta)
-
-    def forward(self) -> Point:
-        return self.delta[self.idx]
 
 
 class App:
