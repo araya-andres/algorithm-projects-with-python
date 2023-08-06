e48f0df2d12afc071f3f7bf03e47e86832515045
Andres Araya <araya.andres@gmail.com>
2023-08-06 01:52:34 +0000

    task class

--- 3-scheduling/task.py
+++ 3-scheduling/task.py
@@ -5,6 +5,13 @@ from typing import List
 
 class Task:
     def __init__(self, name: str, index: int, prereq_numbers: List[int]):
-        self.name = name
-        self.index = index
-        self.prereq_numbers = prereq_numbers
+        self.name: str = name
+        self.index: int = index
+        self.prereq_numbers: List[int] = prereq_numbers
+        self.prereq_tasks: List[Task] = []
+
+    def __str__(self) -> str:
+        return self.name
+
+    def numbers_to_tasks(self, tasks: List[Task]) -> None:
+        self.prereq_tasks = [tasks[index] for index in self.prereq_numbers]
---
+++ 3-scheduling/test_task.py
@@ -0,0 +1,19 @@
+from task import Task
+
+
+def test_task_to_str():
+    task_name = "Dig dungeon"
+    task = Task(task_name, 1, [])
+    assert str(task) == task_name
+
+
+def test_numbers_to_tasks():
+    task_a = Task("Grade site", 0, [])
+    task_b = Task("Dig dungeon", 1, [0])
+    tasks = [task_a, task_b]
+
+    task_a.numbers_to_tasks(tasks)
+    assert task_a.prereq_tasks == []
+
+    task_b.numbers_to_tasks(tasks)
+    assert task_b.prereq_tasks == [task_a]
